c77f34573710a76771e5dfc0d32fe321157a4511
Abnersouza7 <65561500+Abnersouza7@users.noreply.github.com>
2022-09-08 16:25:34 -0700

    Add files via upload

--- maze.py
+++ maze.py
@@ -1,4 +1,7 @@
 
+"""foi desenvolvido no trabalho a matriz de teste para o labirinto,
+a leitura de posicao atual, uma funcao de teste de objetivo e implementado o algoritmo de DFS"""
+
 """variaveis
 
 ordem_matriz                            ok
@@ -24,17 +27,26 @@ custo de caminho
 custo de passo
 """
 
+
+
+
+from collections import defaultdict
 from logging.handlers import QueueListener
 from platform import node
-
-
+from re import S
 class Maze:
     def __init__(self, matriz):
         self.ordem = len(matriz)
         self.matriz = matriz
         self.find_pos_inicio()
         self.find_pos_fim()
-        self.pos_atual = self.pos_inicio
+        self.caminho = []
+
+        print("DFS: ")
+        self.dfs(self.pos_inicio)
+        #for k in self.caminho:
+        #    print(k)
+        # self.bfs(self.pos_inicio)
 
     def find_pos_inicio(self):
         for i in range(self.ordem):  # linha
@@ -50,50 +62,96 @@ class Maze:
                     self.pos_fim = [i, j]
         print("Pos fim: ", self.pos_fim)
 
-    def funcao_sucessor(self):
-        # coord = [linha, coluna] | direita = [ , +] , esquerda = [ , i], cima = [ , -], baixo = [ , +]
-        x = self.pos_atual[0]
-        y = self.pos_atual[1]
-        self.lista_aux = []
-        if self.matriz[x, y + 1] == 1 and y != 9 or self.matriz[x, y - 1] == 1 and y != 0 or self.matriz[x + 1, y] == 1 and x != 9 or self.matriz[x - 1, y] == 1 and x != 0:
-            self.lista.append([[x, y], [0]])
-            self.lista_aux.append([[x, y], [0]])
+    def teste_objetivo(self, next):
+        y = next[0]
+        x = next[1]
 
-    def teste_objetivo(self):
-        # coord = [linha, coluna] | direita = [ , +] , esquerda = [ , i], cima = [ , -], baixo = [ , +]
-        x = self.pos_atual[0]
-        y = self.pos_atual[1]
+        if x < self.ordem - 1:
+            if self.matriz[y][x + 1] == 3:
+                next = [y, x + 1]
+                return 1
 
-        if self.matriz[x, y + 1] == 3 and y != 9 or self.matriz[x, y - 1] == 3 and y != 0 or self.matriz[x + 1, y] == 3 and x != 9 or self.matriz[x - 1, y] == 3 and x != 0:
-            return 0
-        return 1
+        if x >= 0:
+            if self.matriz[y][x - 1] == 3:
+                next = [y, x - 1]
+                return 1
 
-    def bfs(self):
+        if y < self.ordem - 1:
+            if self.matriz[y + 1][x] == 3:
+                next = [y + 1, x]
+                return 1
+
+        if y >= 0:
+            if self.matriz[y - 1][x] == 3:
+                next = [y - 1, x]
+                return 1
+
+        return 0
+
+    def dfs(self, next):
         # toda vez que der um passo, chamar funcao_sucessor E pos_atual att q ja percorreu -> lista[1] = 1 E self.caminho.append(pos_atual)
-        while (self.teste_objetivo()):
-            for i in self.lista:
-                if i[1] == 0:
-                    self.pos_atual = i[0]
-                    i[1] = 1
-                    self.funcao_sucessor()
-                    #######
-                    self.caminho.append(i[0])
 
-        # objetivo funcao: traçar caminho
-        print("bla")
+        if (next not in self.caminho):
+            print(next)
+            self.caminho.append(next)
+            if (self.teste_objetivo(next)):
+                print("FIM", self.pos_fim)
+                self.caminho.append(self.pos_fim)
+                return 1
+
+            y = next[0]
+            x = next[1]
+
+            if x >= 0: # esquerda
+                if self.matriz[y][x - 1] == 1:
+                    next = [y, x - 1]
+                    self.dfs(next)
+
+            if y < self.ordem - 1: # baixo
+                if self.matriz[y + 1][x] == 1:
+                    next = [y + 1, x]
+                    self.dfs(next)
+            
+            if x < self.ordem - 1: # direita
+                if self.matriz[y][x + 1] == 1:
+                    next = [y, x + 1]
+                    self.dfs(next)
+
+            if y >= 0: # cima
+                if self.matriz[y - 1][x] == 1:
+                    next = [y - 1, x]
+                    self.dfs(next)
+
+    def bfs(self, s):
+        self.visited = [False] * (max(self.matriz) + 1)
+
+        self.queue = []
+
+        self.queue.append(s)
+        self.visited[s] = True
+
+        while self.queue:
+            s = self.queue.pop(0)
+            print(s, end=" ")
+            for i in self.matriz[s]:
+                if self.visited[i] == False:
+                    self.queue.append(i)
+                    self.visited[i] = True
 
 
 if __name__ == "__main__":
-
-    matriz = [[2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-              [0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
-              [0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
-              [0, 0, 1, 1, 1, 1, 0, 1, 0, 0],
-              [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-              [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-              [0, 0, 1, 3, 1, 1, 1, 1, 0, 0]]
+    # aaaaaaaa#0 #1 #2 #3 #4 #5 #6 #7 #8 #9
+    matriz = [[2, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 0
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 1
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 2
+              [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],  # 3
+              [0, 0, 0, 1, 0, 1, 0, 1, 0, 0],  # 4
+              [0, 0, 0, 1, 0, 1, 0, 1, 0, 0],  # 5
+              [0, 0, 1, 1, 1, 1, 0, 1, 0, 0],  # 6
+              [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],  # 7
+              [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],  # 8
+              [0, 0, 1, 3, 1, 1, 1, 1, 0, 0]]  # 9
+    for i in matriz:
+        print(i)
 
     maze = Maze(matriz)
